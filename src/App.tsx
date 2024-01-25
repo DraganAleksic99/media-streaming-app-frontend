2bfd583a4ed9463ec9e42da0c53f603e7fc600cd
Dragan Aleksic <aleksicdragan574@gmail.com>
2024-01-25 23:24:41 +0100

    Add styles, remove strict mode

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,18 @@
 import './App.css'
 import { BrowserRouter as Router } from 'react-router-dom'
+import { CssBaseline } from '@mui/material'
 import Navigation from './components/Navigation'
 import MainRouter from './Routes'
 
 function App() {
   return (
-    <Router>
-      <Navigation />
-      <MainRouter />
-    </Router>
+    <div className="app">
+      <Router>
+        <CssBaseline />
+        <Navigation />
+        <MainRouter />
+      </Router>
+    </div>
   )
 }
 
--- src/views/media/Media.tsx
+++ src/views/media/Media.tsx
@@ -13,41 +13,50 @@ import {
 import { Edit } from '@mui/icons-material'
 import auth from '../../auth/authHelper'
 import { Link } from 'react-router-dom'
+import { TMedia } from './MediaList'
 import DeleteMedia from './DeleteMedia'
 import MediaPlayer from './MediaPlayer'
 
 const baseUrl = 'http://localhost:3500'
 
-export default function Media({ media, nextUrl = '', handleAutoplay = '' }) {
+type Props = {
+  media: TMedia | Record<string, never>
+  nextUrl?: string
+  handleAutoplay?: string
+}
+
+export default function Media({ media, nextUrl = '', handleAutoplay = '' }: Props) {
   const mediaUrl = media._id ? `${baseUrl}/api/medias/video/${media._id}` : null
   const nexturl = nextUrl
   return (
     <Card>
       <CardHeader
         title={media.title}
-        action={<span>{media.views + ' views'}</span>}
+        action={<span style={{ paddingRight: '10px' }}>{media.views + ' views'}</span>}
         subheader={media.genre}
       />
       <MediaPlayer srcUrl={mediaUrl} nextUrl={nexturl} handleAutoplay={handleAutoplay} />
-      <List dense>
+      <List sx={{ padding: 0 }} dense>
+        <Divider />
         <ListItem>
           <ListItemAvatar>
-            <Avatar>{media.postedBy.name && media.postedBy.name[0]}</Avatar>
+            <Avatar>{media.postedBy?.name && media.postedBy.name[0]}</Avatar>
           </ListItemAvatar>
           <ListItemText
-            primary={media.postedBy.name}
+            primary={media.postedBy?.name}
             secondary={'Published on ' + new Date(media.created).toDateString()}
           />
-          {auth.isAuthenticated().user && auth.isAuthenticated().user._id == media.postedBy._id && (
-            <ListItemSecondaryAction>
-              <Link to={'/media/edit/' + media._id}>
-                <IconButton aria-label="Edit" color="secondary">
-                  <Edit />
-                </IconButton>
-              </Link>
-              <DeleteMedia mediaId={media._id} mediaTitle={media.title} />
-            </ListItemSecondaryAction>
-          )}
+          {auth.isAuthenticated().user &&
+            auth.isAuthenticated().user._id == media.postedBy?._id && (
+              <ListItemSecondaryAction>
+                <Link to={'/media/edit/' + media._id}>
+                  <IconButton aria-label="Edit" color="secondary">
+                    <Edit />
+                  </IconButton>
+                </Link>
+                <DeleteMedia mediaId={media._id} mediaTitle={media.title} />
+              </ListItemSecondaryAction>
+            )}
         </ListItem>
         <Divider />
         <ListItem>
